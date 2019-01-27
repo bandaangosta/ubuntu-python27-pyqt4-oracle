3e5b375bcf2cab6d23896bcc1c905f427ca1c2dc
Jose Ortiz <jlortiz@uc.cl>
2019-01-27 16:19:10 -0300

    For simplification, removes bash script, moving those commands to Dockerfile.

--- instantclient_12.2/install_instantclient.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p /opt/oracle
-unzip ./instantclient-basiclite-linux.x64-12.2.0.1.0.zip -d /opt/oracle/
-unzip ./instantclient-sdk-linux.x64-12.2.0.1.0.zip -d /opt/oracle/
-ln -s /opt/oracle/instantclient_12_2/libclntsh.so.12.1 /opt/oracle/instantclient_12_2/libclntsh.so
-ln -s /opt/oracle/instantclient_12_2/libocci.so.12.1 /opt/oracle/instantclient_12_2/libocci.so
